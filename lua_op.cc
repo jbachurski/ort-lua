36733b32061c862824412095358efdf95de9ba4d
jbachurski <kbachurski@gmail.com>
2022-12-23 02:07:30 +0100

    Richer support for tensors as input

--- lua_op.cc
+++ lua_op.cc
@@ -17,31 +17,88 @@ void LuaKernel::Compute(OrtKernelContext* context) {
     std::string message(lua_tostring(lua_state, -1));
     throw std::runtime_error(message);
   }
-  if(!lua_gettop(lua_state)) {
-    throw std::runtime_error("Lua code must return a function, but nothing was returned.");
+  if(lua_gettop(lua_state) != 1) {
+    throw std::runtime_error("Lua code must return a single function, but " + std::to_string(lua_gettop(lua_state)) +  " values were returned.");
   }
   if(!lua_isfunction(lua_state, -1)) {
     throw std::runtime_error("Lua code must return a function to be called with operator inputs, but a different type was found.");
   }
   // Bottom of stack is now the compute function
 
+  lua_checkstack(lua_state, 2 * OP_IO_SLOTS);
   // Parse input data and place on Lua stack as tables
-  // TODO: Non-flattened input.
   // TODO: Non-number input.
+  std::vector<int64_t> shapes[OP_IO_SLOTS];
   for(size_t k = 0; k < OP_IO_SLOTS; k++) {
-    const OrtValue* value = ort_.KernelContext_GetInput(context, 0);
+    const OrtValue* value = ort_.KernelContext_GetInput(context, k);
+    if(value == nullptr) {
+      lua_pushnil(lua_state);
+      continue;
+    }
+    auto& shape = shapes[k];
     OrtTensorTypeAndShapeInfo* info = ort_.GetTensorTypeAndShape(value);
-    auto shape = ort_.GetTensorShape(info);
+    shape = ort_.GetTensorShape(info);
     ort_.ReleaseTensorTypeAndShapeInfo(info);
     const double* data = reinterpret_cast<const double*>(ort_.GetTensorData<double>(value));
 
-    size_t X_size = 1;
-    for(auto x : shape)
-      X_size *= x;
-    lua_createtable(lua_state, X_size, 0);
-    for(size_t i = 0; i < X_size; i++) {
-      lua_pushnumber(lua_state, i + 1);
-      lua_pushnumber(lua_state, data[i]);
+    size_t size = 1;
+    for(auto d : shape) size *= d;
+
+    lua_createtable(lua_state, 4, 0); {
+      lua_pushstring(lua_state, "index");
+      lua_pushnumber(lua_state, k);
+      lua_settable(lua_state, -3);
+
+      lua_pushstring(lua_state, "ptr");
+      lua_pushlightuserdata(lua_state, (void*)data);
+      lua_settable(lua_state, -3);
+
+      lua_pushstring(lua_state, "shape");
+      lua_createtable(lua_state, shape.size(), 0);
+      for(size_t i = 0; i < shape.size(); i++) {
+        lua_pushnumber(lua_state, i + 1);
+        lua_pushnumber(lua_state, shape[i]);
+        lua_settable(lua_state, -3);
+      }
+      lua_settable(lua_state, -3);
+
+      lua_pushstring(lua_state, "get");
+      lua_pushlightuserdata(lua_state, (void*)data);
+      lua_pushinteger(lua_state, size);
+      lua_pushlightuserdata(lua_state, (void*)&shape);
+      lua_pushcclosure(lua_state, [](lua_State *L1) {
+        double* data = reinterpret_cast<double*>(lua_touserdata(L1, lua_upvalueindex(1)));
+        size_t size = lua_tonumber(L1, lua_upvalueindex(2));
+        std::vector<int64_t>& shape = *reinterpret_cast<std::vector<int64_t>*>(lua_touserdata(L1, lua_upvalueindex(3)));
+        if (lua_gettop(L1) != shape.size()) {
+          return luaL_error(L1, "Tensor get: expected number of arguments to be equal to rank.");
+        }
+        size_t index = 0;
+        for(size_t i = 0; i < shape.size(); i++) {
+          index *= shape[i];
+          index += luaL_checkinteger(L1, 1 + i);
+        }
+        if(index >= size) {
+          return luaL_error(L1, "Tensor get: index out of bounds.");
+        }
+        lua_pushnumber(L1, data[index]);
+        return 1;
+      }, 3);
+      lua_settable(lua_state, -3);
+
+      lua_pushstring(lua_state, "memget");
+      lua_pushlightuserdata(lua_state, (void*)data);
+      lua_pushinteger(lua_state, size);
+      lua_pushcclosure(lua_state, [](lua_State *L1) {
+        double* data = reinterpret_cast<double*>(lua_touserdata(L1, lua_upvalueindex(1)));
+        size_t size = lua_tonumber(L1, lua_upvalueindex(2));
+        auto index = luaL_checkinteger(L1, 1);
+        if(index >= size) {
+          return luaL_error(L1, "Tensor get: index out of bounds.");
+        }
+        lua_pushnumber(L1, data[index]);
+        return 1;
+      }, 2);
       lua_settable(lua_state, -3);
     }
   }
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import time
 import numpy
 import onnx
 import onnx.helper
@@ -9,8 +10,8 @@ print(f'{onnxruntime.__version__ = }')
 test_code = """
 function run(xs, ys)
     local x = 0
-    for k, v in ipairs(xs) do
-        x = x + v * ys[k]
+    for i = 0, xs.shape[1] - 1 do
+        x = x + xs.get(i) * (ys.get(0, i) + ys.get(1, i))
     end
     return x, -x
 end
@@ -29,7 +30,7 @@ model = onnx.helper.make_model(
         )],
         'graph', [
             onnx.helper.make_tensor_value_info('x', onnx.TensorProto.DOUBLE, ('N')),
-            onnx.helper.make_tensor_value_info('y', onnx.TensorProto.DOUBLE, ('N'))
+            onnx.helper.make_tensor_value_info('y', onnx.TensorProto.DOUBLE, (2, 'N'))
         ], [
             onnx.helper.make_tensor_value_info('r', onnx.TensorProto.DOUBLE, ()),
             onnx.helper.make_tensor_value_info('s', onnx.TensorProto.DOUBLE, ())
@@ -47,5 +48,22 @@ session = onnxruntime.InferenceSession(model.SerializeToString(), options)
 
 print(session.run(None, {
     'x': numpy.array([1.0, 2.0, 3.0]),
-    'y': numpy.array([-1.0, 0.5, 1.0])
+    'y': numpy.array([[-1.0, 0.5, 1.0], [-1.0, 0.5, 1.0]])
 }))
+
+xn = numpy.random.randn(10**6)
+yn = numpy.random.randn(2, 10**6)
+start = time.time()
+print(session.run(None, {
+    'x': xn,
+    'y': yn
+}))
+print(time.time() - start)
+start = time.time()
+_ = xn * (yn[0] + yn[1])
+print(time.time() - start)
+start = time.time()
+s = 0
+for i in range(len(xn)):
+    s += xn[i] * (yn[0,i] + yn[1,i])
+print(time.time() - start)
